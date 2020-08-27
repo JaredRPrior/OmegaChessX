66078541d9eaab7001227c86638b6ae875f6da3d
Jared <jrprior@bowdoin.edu>
2020-08-27 13:12:36 -0400

    push

---
+++ omegaGambit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  omegaGambit
+//
+//  Created by Jared Prior on 8/27/20.
+//  Copyright © 2020 Jared Prior. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
